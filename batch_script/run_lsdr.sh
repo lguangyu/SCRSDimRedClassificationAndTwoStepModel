ac5d34ee982faf092bbefedcc15b19d36d0f214b
Guangyu Li <li.gua@husky.neu.edu>
2019-01-08 14:00:24 -0500

    code refactor

--- batch_script/run_lsdr.sh
+++ batch_script/run_lsdr.sh
@@ -3,11 +3,10 @@
 
 for model in {gnb,lr,lda,svm_lin,svm_rbf}; do
 	for file in {EXPONENT1-50,PLATFORM1-50,PLATFORM2-50}; do
-		for dimred in {none,}; do
-			python3 ./script/t1_methods.py \
-				--data ./data/$file".normalized_l2.data.tsv" \
-				--meta ./data/$file".normalized_l2.meta.tsv" \
-				--classifier $model --dim-reduc $dimred
-		done
+		python3 ./script/t1_methods.py \
+			--data ./data/$file".normalized_l2.data.tsv" \
+			--meta ./data/$file".normalized_l2.meta.tsv" \
+			--classifier $model \
+			--dim-reduc lsdr --reduce-dim-to 26
 	done
 done
