edba6777139f865235f06d63544f7c416cfcf3ba
Guangyu Li <li.gua@husky.neu.edu>
2019-08-17 16:02:53 -0400

    added cv to select C-SVM loss coefficient (see libsvm)

--- script/pylib/classifier.py
+++ script/pylib/classifier.py
@@ -5,9 +5,11 @@ import numpy
 import sklearn.discriminant_analysis
 import sklearn.linear_model
 import sklearn.metrics
+import sklearn.model_selection
 import sklearn.naive_bayes
 import sklearn.svm
 from . import base_class
+from . import result_evaluate
 
 
 _CLASSIFIERS = dict()
@@ -77,13 +79,69 @@ class KSVM_RBF(SklearnClassifierAliasMethodsMixin,\
 		super(KSVM_RBF, self).__init__(kernel = "rbf", **kw)
 		return
 
-	def train(self, X, *ka, **kw):
+	def train(self, X, Y, *ka, **kw):
 		# for rbf svm, gamma should be calculated
 		sigma = numpy.median(sklearn.metrics.pairwise.euclidean_distances(X))
 		gamma = 1.0 / (2 * sigma * sigma)
 		self.set_params(gamma = gamma)
 		assert self.get_params()["gamma"] == gamma
-		return super(KSVM_RBF, self).train(X, *ka, **kw)
+		return super(KSVM_RBF, self).train(X, Y, *ka, **kw)
+
+
+@register_classifier("svm_lin_cv")
+class SVMLinearCV(SVMLinear):
+	def train(self, X, Y, *ka, **kw):
+		c_list = [0.001, 0.01, 0.1, 1.0, 10, 100, 1000]
+		# inner loop to choose C
+		best_c, best_acc = None, None
+		cv = sklearn.model_selection.StratifiedKFold(n_splits = 5)
+		for c in c_list:
+			round_acc = list()
+			for train, test in cv.split(X, Y):
+				# train with
+				self.set_params(C = c)
+				super(SVMLinearCV, self).train(X[train], Y[train], *ka, **kw)
+				pred = self.predict(X[test])
+				acc = result_evaluate.LabelPredictEvaluate(pred_label = pred,
+					true_label = Y[test])["average_accuracy"]
+				round_acc.append(acc)
+			# select best
+			acc = numpy.mean(round_acc)
+			if (best_acc is None) or (acc > best_acc):
+				best_acc = acc
+				best_c = c
+		# retrain with best c
+		self.set_params(C = c)
+		self.train(X, Y)
+		return
+
+
+@register_classifier("svm_rbf_cv")
+class KSVM_RBF_CV(KSVM_RBF):
+	def train(self, X, Y, *ka, **kw):
+		c_list = [0.001, 0.01, 0.1, 1.0, 10, 100, 1000]
+		# inner loop to choose C
+		best_c, best_acc = None, None
+		cv = sklearn.model_selection.StratifiedKFold(n_splits = 5)
+		for c in c_list:
+			round_acc = list()
+			for train, test in cv.split(X, Y):
+				# train with
+				self.set_params(C = c)
+				super(KSVM_RBF_CV, self).train(X[train], Y[train], *ka, **kw)
+				pred = self.predict(X[test])
+				acc = result_evaluate.LabelPredictEvaluate(pred_label = pred,
+					true_label = Y[test])["average_accuracy"]
+				round_acc.append(acc)
+			# select best
+			acc = numpy.mean(round_acc)
+			if (best_acc is None) or (acc > best_acc):
+				best_acc = acc
+				best_c = c
+		# retrain with best c
+		self.set_params(C = c)
+		self.train(X, Y)
+		return
 
 
 def create(registed_name, *ka, **kw):
