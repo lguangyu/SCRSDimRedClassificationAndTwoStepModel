18b229822a69bbf61f5683c9e03a54586ddbde71
Guangyu Li <gl343@cornell.edu>
2024-02-23 11:02:47 -0500

    coma-tet datasets

--- script/pylib/dataset.py
+++ script/pylib/dataset.py
@@ -72,6 +72,9 @@ class DatasetBase(object):
 	def pp_scale(self, data, *ka, **kw):
 		return sklearn.preprocessing.scale(data, *ka, **kw)
 
+	def pp_normalize(self, data, *ka, norm="l2", **kw):
+		return sklearn.preprocessing.normalize(data, *ka, norm=norm, **kw)
+
 	def pp_filter(self, cond, *data, axis=0, **kw):
 		return tuple([numpy.compress(cond, d, axis=axis, **kw) for d in data])
 
@@ -90,7 +93,22 @@ class SingleLabelDatasetBase(DatasetBase):
 	text_label:		labels in text format
 	label_encoder:	LabelEncoder instance fitted by <text_label>
 	"""
-	pass
+
+	def sync_label_with(self, other: "SingleLabelDatasetBase"):
+		if not isinstance(other, SingleLabelDatasetBase):
+			raise TypeError("argument must be an instance of %s, not %s"
+				% (SingleLabelDatasetBase.__name__, type(other).__name__))
+		# sync self
+		all_uniq_label = sorted(set.union(
+			set(self.text_label), set(other.text_label)))
+		self.label_encoder = sklearn.preprocessing.LabelEncoder()
+		self.label_encoder.fit(all_uniq_label)
+		self.label = self.label_encoder.transform(self.text_label)
+		# sync other
+		other.label_encoder = sklearn.preprocessing.LabelEncoder()
+		other.label_encoder.fit(all_uniq_label)
+		other.label = other.label_encoder.transform(other.text_label)
+		return
 
 
 class DuoLabelDatasetBase(DatasetBase):
@@ -272,4 +290,34 @@ class ZijianExpStaDuoLabelDataset(DuoLabelDatasetBase):
 		self.strain_text_label = self._strain_text_label
 		self.phase_label = self.phase_label_encoder.transform(_phase_text_label)
 		self.strain_label = self.strain_label_encoder.transform(_strain_text_label)
+		return
+
+
+@DatasetCollection.register("coma-tet-pure-strain-only")
+class ComaTetPureStrainLabelDataset(SingleLabelDatasetBase):
+	_raw_data_file_ = "./data/coma_tet_pure.normalized_l2.data.tsv"
+	_raw_label_file_ = "./data/coma_tet_pure.labels.txt"
+
+	def __init__(self, *ka, **kw):
+		super().__init__(*ka, **kw)
+		self.data_no_scale = self.raw_data
+		self.data = self.pp_normalize(self.pp_scale(self.raw_data), norm="l2")
+		self.text_label = self.raw_strain_label
+		self.label_encoder = self.strain_label_encoder
+		self.label = self.label_encoder.transform(self.text_label)
+		return
+
+
+@DatasetCollection.register("coma-tet-ramanfish-strain-only")
+class ComaTetRamanfishStrainLabelDataset(SingleLabelDatasetBase):
+	_raw_data_file_ = "./data/coma_tet_raman-fish.normalized_l2.data.tsv"
+	_raw_label_file_ = "./data/coma_tet_raman-fish.labels.txt"
+
+	def __init__(self, *ka, **kw):
+		super().__init__(*ka, **kw)
+		self.data_no_scale = self.raw_data
+		self.data = self.pp_normalize(self.pp_scale(self.raw_data), norm="l2")
+		self.text_label = self.raw_strain_label
+		self.label_encoder = self.strain_label_encoder
+		self.label = self.label_encoder.transform(self.text_label)
 		return
